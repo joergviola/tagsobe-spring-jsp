b3a9f5f7daf5f223546872c99bba471669ba684d
Jörg Viola <joerg@joergviola.de>
2011-12-19 10:35:03 +0100

    wait for tomcat

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@
 mvn install
 sudo cp target/travel.war $TOMCAT_HOME/webapps
 sudo service tomcat6 start
-sleep 5
+until [ "`curl --silent --show-error --connect-timeout 1 -I http://localhost:8080 | grep 'Coyote'`" != "" ];
+do
+  echo Waiting for tomcat coming up...
+  sleep 2
+done
+
 
 echo "http://localhost:8080/travel/hotels/search" > probe.url

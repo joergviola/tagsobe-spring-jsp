1d76250aeee6bf10315c0faa55379e609953ee33
Jörg Viola <joerg@joergviola.de>
2011-12-19 10:27:02 +0100

    write url to file

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ sudo cp target/travel.war $TOMCAT_HOME/webapps
 sudo service tomcat6 start
 sleep 5
 
-export PROBE_URL=http://localhost:8080/travel/hotels/search
+echo "http://localhost:8080/travel/hotels/search" > probe.url
